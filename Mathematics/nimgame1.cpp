38ef28dd8418978b96a239d0d1e943c21f27df63
Yash Johri <yashjohri1200@gmail.com>
2020-05-25 13:46:34 +0530

    More sols

--- Mathematics/nimgame1.cpp
+++ Mathematics/nimgame1.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 typedef long long int ll;
 #define mod 1000000007
+void getSetBitsFromOneToN(ll N){
+	ll two=2,ans=0;
+	ll n=N;
+	while(n){
+		ans+=(N/two)*(two>>1);
+		if((N&(two-1))>(two>>1)-1)
+			ans+=(N&(two-1))-(two>>1)+1;
+		two<<=1;
+		n>>=1;
+	}
+	cout<<ans<<endl;
+}
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t,n,a;
-	cin>>t;
-	while(t--){
-		int res=0;
-		cin>>n;
-		for(int i=0;i<n;i++)
-			cin>>a,res^=a;
-		if(res==0)
-			cout<<"second\n";
-		else
-			cout<<"first\n";
-	}
+	ll n;
+	cin>>n;
+	getSetBitsFromOneToN(n);
 	return 0;
 }
